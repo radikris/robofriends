97a40cc120d0c460bfb587b412db39bee46e87dd
radikris <radi.kristof@gmail.com>
2021-01-17 17:32:53 +0100

    first commit

---
+++ src/components/ErrorBoundry.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+class ErrorBoundry extends Component {
+    constructor(props) {
+        super(props);
+        this.setState({
+            hasError: false
+        })
+    }
+
+
+    componentDidCatch(error, info){
+        this.setState({
+            hasError: true
+        })
+    }
+
+    render() {
+        if (this.state.hasError) {
+            return <h1>Oooops. That is not good</h1>
+        } else {
+            return this.props.children;
+        }
+
+
+    }
+}
+
+export default ErrorBoundry
---
+++ src/components/SearchBox.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SearchBox = ({searchfield, searchChange}) => {
+    return (
+        <input type="search" placeholder='search robots' onChange={searchChange}/>
+    );
+
+}
+export default SearchBox;
