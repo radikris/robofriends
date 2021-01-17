d1c775dfa3e8f7d832d5a6e5182bc3103e82a5e8
radikris <radi.kristof@gmail.com>
2021-01-17 17:47:00 +0100

    Errorbundary update

--- src/components/ErrorBoundry.js
+++ src/components/ErrorBoundry.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react'
 class ErrorBoundry extends Component {
     constructor(props) {
         super(props);
-        this.setState({
+        this.state = {
             hasError: false
-        })
+        }
     }
 
 
