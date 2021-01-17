5b541562e17420c0d927d520e565c195ec80910b
radikris <radi.kristof@gmail.com>
2021-01-17 18:23:39 +0100

    Updates

--- src/components/SearchBox.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const SearchBox = ({searchfield, searchChange}) => {
-    return (
-        <input type="search" placeholder='search robots' onChange={searchChange}/>
-    );
-
-}
-export default SearchBox;
